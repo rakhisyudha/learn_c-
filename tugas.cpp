e75bfd2534820fc0a12d68c308904d71009ce36a
Rakhis Yudha <rakhisyuda@gmail.com>
2024-09-21 18:47:43 +0700

    lanjutan

--- tugas.cpp
+++ tugas.cpp
@@ -1,9 +1,28 @@
-#include <stdio.h>
+/* #include <stdio.h>
 
 int main()
 {
     int testInteger = 5;
     testInteger = testInteger * 2 + 3; // Added operation: multiplication and addition
     printf("Number = %d\n", testInteger);
+    return 0;
+}
+*/
+#include <stdio.h>
+
+int main() {
+    int fib[10];
+    int *ptr = fib;
+    *ptr = 0;
+    *(ptr + 1) = 1;
+
+    for(int i = 2; i < 10; i++) {
+        *(ptr + i) = *(ptr + i - 1) + *(ptr + i - 2); // Mistake here!
+    }
+
+    for(int i = 0; i < 10; i++) {
+        printf("%d ", *(ptr + i));
+    }
+
     return 0;
 }
